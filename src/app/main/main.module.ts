386123448069e12e64a6c82c6e60e7f83ad60310
alisherabbasii <alisher2161@gmail.com>
2022-04-07 19:18:55 +0500

    latest changes so far 07-04

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -7,12 +7,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderWithLogoComponent } from './components/header-with-logo/header-with-logo.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     BodyComponent,
+    FooterComponent,
+    HeaderWithLogoComponent,
   ],
   imports: [
     CommonModule,
