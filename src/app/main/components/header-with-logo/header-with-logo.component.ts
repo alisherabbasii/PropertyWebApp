4471b5c8bb86e6eaa8a9505ee0e992c4ccf1d7d3
alisherabbasii <alisher2161@gmail.com>
2022-04-15 19:52:44 +0500

    header animation etc changes

--- src/app/main/components/header-with-logo/header-with-logo.component.ts
+++ src/app/main/components/header-with-logo/header-with-logo.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-header-with-logo',
   templateUrl: './header-with-logo.component.html',
-  styleUrls: ['./header-with-logo.component.css']
+  styleUrls: ['./header-with-logo.component.css'],
+  animations: [
+    trigger('widthGrow', [
+        state('closed', style({
+            width: 0,
+        })),
+        state('open', style({
+            width: 300
+        })),
+        transition('* => *', animate(350))
+    ]),
+]
 })
 export class HeaderWithLogoComponent implements OnInit {
 
-  showBuyOptions:boolean=false;
+  state= 'closed';
   constructor() { }
 
   ngOnInit(): void {
   }
-  expandHome(){
-   this.showBuyOptions = !this.showBuyOptions;
+
+  expandHome(): void {
+    (this.state == "closed") ? this.state = "open" : this.state = "closed";
   }
+  // expandHome(){
+  //  this.showBuyOptions = !this.showBuyOptions;
+  // }
 }
